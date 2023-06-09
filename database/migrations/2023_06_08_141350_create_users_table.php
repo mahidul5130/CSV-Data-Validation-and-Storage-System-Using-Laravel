103095bfb9b99cf05029d0c2f46ea04d31a6827d
Md. Mahidul Haque <mahidul5130@gmail.com>
2023-06-09 13:00:31 +0600

    master

--- database/migrations/2023_06_08_141350_create_users_table.php
+++ database/migrations/2023_06_08_141350_create_users_table.php
@@ -8,19 +8,21 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
+        // Create the 'users' table
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone_number')->unique();
-            $table->string('gender');
-            $table->string('address');
-            $table->timestamps();
+            $table->id(); // Auto-incrementing ID column
+            $table->string('name'); // Name column
+            $table->string('email')->unique(); // Email column with unique constraint
+            $table->string('phone_number')->unique(); // Phone number column with unique constraint
+            $table->string('gender'); // Gender column
+            $table->string('address'); // Address column
+            $table->timestamps(); // Created_at and updated_at columns for tracking timestamps
         });
     }
 
     public function down()
     {
+        // Drop the 'users' table if it exists
         Schema::dropIfExists('users');
     }
 }
